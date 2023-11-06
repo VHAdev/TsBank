09b77cbaf6af15674873822bcc092ab48e82f354
Vitor Hugo Ayres <vitorayres1997@hotmail.com>
2023-11-06 18:36:03 -0300

    instanciando os 3 tipos de conta no app.ts

--- app.ts
+++ app.ts
@@ -2,7 +2,18 @@ import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 import { YieldAccount } from './class/YieldAccount'
 
+const peopleaccount: PeopleAccount = new PeopleAccount(1, 'V', 10)
+console.log(peopleaccount)
+peopleaccount.deposit(100)
+peopleaccount.withdraw(90)
+console.log(peopleaccount)
 
-const yieldaccount: YieldAccount = new YieldAccount ('V', 2)
-yieldaccount.deposit(50)
-console.log(yieldaccount)
+
+// const companyaccount: CompanyAccount = new CompanyAccount('V', 2)
+// companyaccount.getLoan(500)
+// console.log(companyaccount)
+
+
+// const yieldaccount: YieldAccount = new YieldAccount ('V', 3)
+// yieldaccount.deposit(50)
+// console.log(yieldaccount)
